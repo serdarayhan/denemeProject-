c8be4cbcfa289567ba5e006a4b4bc04444fd0c53
Serdar <serdarrayhann@gmail.com>
2021-04-10 18:04:00 +0200

    Serdar branchi ilk Messaj

--- src/deneme/Gurup_1.java
+++ src/deneme/Gurup_1.java
@@ -4,7 +4,7 @@ public class Gurup_1 {
 
 	public static void main(String[] args) {
 		 int sayi;   //; yazim dilindeki . gibidir.java ;'u gorunce o satirdaki kod yaziliminin bittigini anlar
-	        sayi=27;
+	        sayi=100;
 	        
 	        System.out.print(sayi);//Eger println degilde print yazarsak yazdirma isleminden sonra alt satira gecmez
 	        
@@ -15,6 +15,7 @@ public class Gurup_1 {
 	        double sayiDouble = 5.34;
 	        
 	        System.out.println(sayiDouble);
+	        System.out.println("Serdar Branch içindeki Version 3");
 	        
 
 	}
